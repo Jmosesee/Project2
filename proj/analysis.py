1913ef574f3e67e7ad61ccd8b13985942dfbea63
EC2 Default User <ec2-user@ip-172-31-15-93.us-east-2.compute.internal>
2018-09-18 16:27:03 +0000

    bug fixes

--- proj/analysis.py
+++ proj/analysis.py
@@ -5,69 +5,55 @@ from nltk.tokenize import word_tokenize
 from collections import defaultdict
 from dynamodb_json import json_util as json
 from nltk.tokenize import RegexpTokenizer
+import re
 
-# def load_skills_list(dynamodb):
-#     skills_df = pd.DataFrame(json.loads(dynamodb.Table('Skills').scan()['Items'])).fillna(False)
-#     # skills.columns = ['skill_name']
-#     skills['skill_name'] = skills.skill_name.map(lambda x: x.lower().strip())
-#     return skills.skill_name.tolist()
-
-def load_skills_df(dynamodb):# the macro enabled excel file. 
-    skills = pd.DataFrame(json.loads(dynamodb.Table('Skills').scan()['Items'])).fillna(False)
-    # tokenizer = RegexpTokenizer(r'[a-zA-Z0-9#+-]+')
-    # if not skills.empty:
-    #     skills['WL'] = [tokenizer.tokenize(skill_phrase) for skill_phrase in skills.get('skill_name')]
-
-    return skills
-    
 # job_summary is a string
-def tally_skill_mentions_in_job_summary(job_summary, skills):
+def tally_skill_mentions_in_job(job_summary, job_title, skills):
 
-    tokenizer = RegexpTokenizer(r'[a-zA-Z0-9#+-]+')
-    if skills.empty:
-        return {}
-    skills_wl = [tokenizer.tokenize(skill_phrase) for skill_phrase in skills.get('skill_name')]
-    skill_dict = {skill_phrase: 0 for skill_phrase in skills.get('skill_name')}
-    # skill_mentions_in_job = defaultdict(int)
-    # tokenize the text of the description, without spans
-    tokenizer = RegexpTokenizer(r'[a-zA-Z0-9#+-]+')
-    tokens = tokenizer.tokenize(job_summary)
-    # create a dictionary of the words in the job description
-    word_index = defaultdict(list)
-    for i, k in enumerate(tokens):
-        word_index[k].append(i)
+    skill_dict = {}
+    # print ("Skills: ")
+    # print (skills)
+    for index, row in skills.iterrows():
+        s = row['skill_name']
+        # print (s)
+        pattern = re.compile(s + r'[^\w]', flags=re.I)
+        # print (pattern)
+        skill_dict[s] = len(re.findall(pattern, job_title)) + len(re.findall(pattern, job_summary))
     
-    # search the word_index dictionary to find the first word of each skill_phrase
-    for skill_phrase, skill_phrase_wl in zip(skills.get('skill_name'), skills_wl):
-        if word_index.get(skill_phrase_wl[0]):
-            for occurence in word_index.get(skill_phrase_wl[0]):
-                # Check to see if the whole phrase matches
-                if len(tokens) > (occurence + len(skill_phrase_wl)):
-                    if all((skill_phrase_wl[j] == tokens[j+occurence]) for j in range(len(skill_phrase_wl))):
-                        skill_dict[skill_phrase] += 1
+    # print (skill_dict)
     return skill_dict
 
 def analyze(job, skills, analysis_table):
+    # print ('Beginning job analysis')
     tallied_skill_mentions = []
     job_summary = job['job_summary']
+    job_title = job['jobtitle']
     if type(job_summary) == str:
-        tallied_skill_mentions = tally_skill_mentions_in_job_summary(job_summary, skills)
+        tallied_skill_mentions = tally_skill_mentions_in_job(job_summary, job_title, skills)
         table_item = tallied_skill_mentions.copy()  # Shallow copy!  (Deep copy would be fine too)
         table_item['JobId'] = job['JobId']
         analysis_table.put_item(Item=table_item)
     else:
         tallied_skill_mentions = defaultdict(int)
+    print (tallied_skill_mentions)
     return tallied_skill_mentions
 
-def reanalyze(analysis_df, skill, jobs_table):
+def reanalyze(skill, jobs_table, analysis_table):
+    # analysis_df is local
+    analysis_df = pd.DataFrame(json.loads(analysis_table.scan()['Items'])).fillna(False)
     reanalysis = []
-    for index, row in analysis_df.iterrows():
-        response = jobs_table.get_item(Key={'JobId': index})
-        job_summary = response['Item']['job_summary']
-        #Todo: also need to reanalyze jobs in jobs_table_queue
-        if type(job_summary) == str:
-            tallied_skill_mentions = tally_skill_mentions_in_job_summary(job_summary, [skill])[skill]
-        else:
-            tallied_skill_mentions = 0
-        reanalysis.append(tallied_skill_mentions)
+    with analysis_table.batch_writer() as batch:
+        for index, row in analysis_df.iterrows():
+            response = jobs_table.get_item(Key={'JobId': index})
+            job_summary = response['Item']['job_summary']
+            job_title = response['Item']['job_summary']
+            #Todo: also need to reanalyze jobs in jobs_table_queue
+            if type(job_summary) == str:
+                tallied_skill_mentions = tally_skill_mentions_in_job(job_summary, job_title, [skill])[skill]
+            else:
+                tallied_skill_mentions = 0
+            reanalysis.append(tallied_skill_mentions)
+            table_item = tallied_skill_mentions.copy()  # Shallow copy!  (Deep copy would be fine too)
+            table_item['JobId'] = index
+            batch.put_item(Item=table_item)
     return reanalysis
--- proj/app.py
+++ proj/app.py
@@ -4,20 +4,26 @@
 # 1: Open a terminal window
 # 2: Enter the following command to activate the PythonData environment:
 #    $ source activate PythonData
-# 3. Navigate into the ~/environment/proj folder
+# 3. Navigate into the project folder
 # 4. Enter the following command to start the Celery worker and beat:
 #    $ celery -A app.celery worker --loglevel=info -B
 # 5: Open a second terminal window
-# 6. Navigate into the ~/environment/proj folder
+# 6. Navigate into the project folder
 # 7: Enter the following command to activate the PythonData environment in the second terminal:
 #    $ source activate PythonData
 # 8: Enter the following command to start the Flask app:
 #    $ python3 app.py
 
+# Here we have a Flask app and a Celery worker in the same file.
+# The two processes have access to the variable names defined globally here
+# However, they do not share memory, so they are using two different copies of those variables!
+
+# Todo: Find a way to simplify the above architecture.
 # Todo: Can we create a boot script that will do the above whenever our virtual machine boots?
 # Todo: Create a readme file for this project
 # Todo: Use Github to manage this code
 # Todo: Consider separating the DynamoDB interface into a new module
+# Todo: Consider applying OOP principles
 
 from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
@@ -30,17 +36,21 @@ from collections import defaultdict
 from dynamodb_json import json_util as json
 
 from scrape import get_job_links_page, get_job
-from analysis import load_skills_df, analyze, reanalyze
+from analysis import analyze, reanalyze
 
 dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
 skill_table = dynamodb.Table('Skills')
 constraints_table = dynamodb.Table('Constraints')
 jobs_table = dynamodb.Table('Jobs')
 jobids_table = dynamodb.Table('JobIds')
-skills_table = dynamodb.Table('Skills')
 analysis_table = dynamodb.Table('Analysis')
+
+# jobs_table_queue is for use by Celery and only Celery.
+# scrape_job() appends to it and put_job() pops from it.
 jobs_table_queue = []  # This list temporarily holds scraped job data in RAM until put_job() gets around to putting it into DynamoDB
-analysis_df = pd.DataFrame()
+
+# Todo: Could skills change after being passed into a celery function?
+# analysis_df = pd.DataFrame()
 
 flask_app = Flask(__name__)
 CORS(flask_app)
@@ -56,16 +66,28 @@ def hello():
     return render_template('index.html', name=None)
 
 def put_skill(skill, have):
+    global skills   # As a reminder, don't do this in a celery function, but only in flask functions.
+    
     sk = {"skill_name": skill, "have": True}
+    print ("Before append:")
+    print (skills)
+    skills = skills.append(sk, ignore_index=True)
+    print ("After append:")
+    print (skills)
     skill_table.put_item(Item=sk)
     return 'Inserted: ' + skill
     
 # The return value for these routes is not being used for anything.  Feel free to change it to something useful
 @flask_app.route("/do-have/<skill>")
 def do_have(skill):
+    # global analysis_df
+    
+    # Todo: consider functionizing the next 3 lines of code
     r = put_skill(skill, True)
-    analysis_df[skill] = reanalyze(analysis_df, skill, jobs_table)
-    scrape.delay(skill)
+    # Todo: How long does reanalyzing take?  Should it be handled by celery?
+    #analysis_df[skill] = reanalyze(analysis_df, skill, jobs_table, analysis_table)
+    reanalyze(skill, jobs_table, analysis_table)
+    scrape.delay(skill, skills)
     return r
 
 @flask_app.route("/dont-have/<skill>")
@@ -75,18 +97,22 @@ def dont_have(skill):
 # This route is used to capture the search constraints
 @flask_app.route("/jobs/")
 def jobs():
-    # return request.query_string
+    # global analysis_df
+    
     constraints = {k: v for k,v in request.args.items()}
     skill = constraints.pop('q')
+    # Todo: consider functionizing the next 3 lines of code
     r = put_skill(skill, True)
-    analysis_df[skill] = reanalyze(analysis_df, skill, jobs_table)
-    scrape.delay(skill)
+    # Todo: How long does reanalyzing take?  Should it be handled by celery?
+    # analysis_df[skill] = reanalyze(analysis_df, skill, jobs_table, analysis_table)
+    # analysis_df.append(scrape.delay(skill, skills.to_json()))
+    reanalyze(skill, jobs_table, analysis_table)
+    scrape.delay(skill, skills.to_json())
     # after popping q from a, the remaining parameters are the constraints
+    # Todo: Also need to strip the start page
     encoded = urlencode(constraints)
     c={'ConstraintId': 1, 'Constraint': encoded}
     constraints_table.put_item(Item=c)
-    # return 'Inserted: ' + ", ".join(skill)
-    #return jsonify(sk)
     return ("OK")
 
 def sort_jobs(df):
@@ -97,14 +123,18 @@ def sort_jobs(df):
 
 @flask_app.route("/get-top-jobs/")
 def get_top_jobs():
-    global analysis_df
+    # global analysis_df
     # analysis_df.drop('Score')
     # analysis_df['Score'] = analysis_df.sum(axis = 1)
     # analysis_df = analysis_df.sort_values('Score', ascending=False)
+    # now analysis_df is local
+    analysis_df = pd.DataFrame(json.loads(analysis_table.scan()['Items'])).fillna(False)
     analysis_df = sort_jobs(analysis_df)
     top_jobs_df = pd.DataFrame()
     # print("Length of analysis_df: "+ str(len(analysis_df)))
     for row in range(0,10):
+        if row >= len(analysis_df):
+            break
         print ("Score: " + str(analysis_df.iloc[row]['Score']))
         JobId = analysis_df.iloc[row]['JobId']
         response = jobs_table.get_item(Key={'JobId': JobId})
@@ -114,7 +144,9 @@ def get_top_jobs():
 
 @flask_app.route("/get-top-skills/")
 def get_top_skills():
-    global analysis_df
+    # global analysis_df
+    # now analysis_df is local
+    analysis_df = pd.DataFrame(json.loads(analysis_table.scan()['Items'])).fillna(False)
     analysis_df = sort_jobs(analysis_df)
     trimmed_df = analysis_df.copy()
     if 'JobId' in trimmed_df.columns:
@@ -126,52 +158,61 @@ def get_top_skills():
     
 @flask_app.route("/get-skills/")
 def get_skills():
-    response = skills_table.scan()
-    skills = response['Items']
+    # response = skill_table.scan()
+    # skills = response['Items']
     print(skills)
-    return jsonify(skills)
-    
+    return skills.to_json()
+
+# Celery functions cannot use global variables.  If they try, they will get their own separate copy of them!    
 
 # The reason for this celery beat is to avoid overloading the very limited throughput capacity of our Amazon DynamoDB.
 # We can always raise the capacity if we want to.  Still, it's nice to have control of it, thus.
 @celery.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
-    sender.add_periodic_task(2.0, put_job.s(), name='beat for put_job')
+    sender.add_periodic_task(3.0, put_job.s(), name='beat for put_job')
 
 # Scrape data about one job from Indeed
 @celery.task(name='app.scrape_job')
-def scrape_job(id, link):
-    global analysis_df
+def scrape_job(id, link, json_skills):
+    skills = pd.read_json(json_skills)  # Pandas dataframes can't be passed directly into celery tasks
     j = get_job(link)
     j['JobId'] = id
     j['link'] = link
     # jobs_table.put_item(Item=j)
     jobs_table_queue.append(j)
+    print("Passing job to analyze")
     d = analyze(j, skills, analysis_table)
-    analysis_df = analysis_df.append(pd.DataFrame(d, index=[id]))
     # if len(d.keys()) == len(d.values()):
     # analysis_df.loc[id] = d
+    return d
+    # return pd.DataFrame(d, index=[id])
 
 #Put one job's data into the jobs table of the database
 @celery.task
 def put_job():
-    # print (len(jobs_table_queue))
-    if len(jobs_table_queue) > 0:
-        j = jobs_table_queue.pop()
-        if (j):
-            jobs_table.put_item(Item = j)
+    print (len(jobs_table_queue))
 
+    if len(jobs_table_queue) > 0:
+        batch_count = 0;
+        with jobs_table.batch_writer() as batch:
+            while len(jobs_table_queue) > 0 and batch_count < 10:
+                j = jobs_table_queue.pop()
+                if (j):
+                    batch.put_item(Item = j)
+                    batch_count += 1
+                    
 # Scrape a list of job links by searching Indeed
 @celery.task(name='app.scrape')
-def scrape(query):
-    
-    if query not in analysis_df.columns:
-        analysis_df[query] = pd.Series([0] * len(analysis_df), index=analysis_df.index)
+def scrape(query, json_skills):
+    # scraped_analysis = pd.DataFrame()
+    print("Commencing scrape")
+    # if query not in analysis_df.columns:
+    #     analysis_df[query] = pd.Series([0] * len(analysis_df), index=analysis_df.index)
 
     response = constraints_table.get_item(Key={'ConstraintId': 1})
     # query = "data scientist"
     constraints = response['Item']['Constraint']
-    for page in range(1,100):
+    for page in range(1,3):
         (links, found_jobs, ids) = get_job_links_page(query, constraints, page)
         zipped = list(zip(ids, links))
         jobs = [{"JobId": i, "link": l} for i,l in zipped]
@@ -182,7 +223,9 @@ def scrape(query):
             for j in jobs:
                 batch.put_item(Item=j)
         for i,l in zipped:
-            response = scrape_job.delay(i,l)
+            d = scrape_job.delay(i, l, json_skills)
+            # Todo: Improve efficiency by gathering these dictionaries into a list and then appending to the dataframe as a batch
+            # scraped_analysis = scraped_analysis.append(pd.DataFrame(d, index=[i]))
         if found_jobs < (page*10):
             break
 
@@ -193,13 +236,16 @@ def scrape(query):
             "last_searched": now}
     # Todo: Consider doing this earlier
     skill_table.put_item(Item=sk)
+    # return scraped_analysis
 
-skills = load_skills_df(dynamodb)
-analysis_df = pd.DataFrame(json.loads(dynamodb.Table('Analysis').scan()['Items'])).fillna(False)
-if not skills.empty:
-    for skill in skills.get('skill_name'):
-        if skill not in analysis_df.columns:
-            analysis_df[skill] = pd.Series([0] * len(analysis_df), index=analysis_df.index)
+# skills is a Flask variable
+skills = pd.DataFrame(json.loads(skill_table.scan()['Items'])).fillna(False)
+# Todo: Make skill_name the index
+# print(skills)
+# if not skills.empty:
+#     for skill in skills.get('skill_name'):
+#         if skill not in analysis_df.columns:
+#             analysis_df[skill] = pd.Series([0] * len(analysis_df), index=analysis_df.index)
 
 if __name__ == "__main__":
     flask_app.run(host="0.0.0.0", port=8080, ssl_context='adhoc')
